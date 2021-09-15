890f68e00b7c757ee9e5269198dcb8a9b165314f
Hector Orozco <hectorjoseorozco@gmail.com>
2021-09-15 14:04:17 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <NavBar />
+
+      <Switch>
+        <Route path="/pinned">
+          <Pinned />
+        </Route>
+        <Route path="/glosary">
+          <Glosary />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/">
+          <Earthquakes />
+        </Route>
+      </Switch>
+
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+function Earthquakes(props) {
+  return <h2>Earthquakes</h2>;
+}
+
+function Pinned(props) {
+  return <h2>Pinned</h2>;
+}
+
+function Glosary(props) {
+  return <h2>Glosary</h2>;
+}
+
+function Contact(props) {
+  return <h2>Contact</h2>;
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
+
+const NavBar = () => {
+
+    let history = useHistory();
+
+    // When Navigation Bar mounts, make the browser navigate to the home page
+    useEffect(() => { 
+        history.push("/"); 
+    }, [history]);
+
+    // State: Array that holds a boolean for each of the options on the Navigation Bar. That boolean
+    // is true if that option is active, false otherwise.
+    const [active, setActive] = useState([true]);
+
+    /**
+     * Activates (changes background and color) the clicked option by setting its value on the state 
+     * to true
+     * @param {Event} e Event with information about the clicked object.
+     */
+    const handleClick = e => {
+        let tempActive = [];
+        tempActive[e.currentTarget.id] = true;
+        setActive(tempActive);
+    }
+
+    const background = activeIndex =>
+        active[activeIndex] ?
+            { backgroundColor: "#3E2723" } :  // Brown 900
+            { backgroundColor: "#5D4037" }    // Brown 700
+
+    const fontWeight = activeIndex =>
+        active[activeIndex] ?
+            { color: "#FFF8E1" } :           // Amber 50
+            { color: "#FFD54F" }             // Amber 300
+
+    // render() {
+    return (
+        <nav className="NavBar">
+            <ul>
+                <li id="0" onClick={handleClick} style={background(0)}>
+                    <Link to="/">
+                        <span style={fontWeight(0)}>Earthquakes</span>
+                    </Link>
+                </li>
+                <li id="1" onClick={handleClick} style={background(1)}>
+                    <Link to="/pinned">
+                        <span style={fontWeight(1)}>Pinned</span>
+                    </Link>
+                </li>
+                <li id="2" onClick={handleClick} style={background(2)}>
+                    <Link to="/glosary">
+                        <span style={fontWeight(2)}>Glosary</span>
+                    </Link>
+                </li>
+                <li id="3" onClick={handleClick} style={background(3)}>
+                    <Link to="/contact">
+                        <span style={fontWeight(3)}>Contact</span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+
+}
+
+export default NavBar;
